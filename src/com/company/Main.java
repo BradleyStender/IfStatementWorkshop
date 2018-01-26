d23be4182d8268b5221ea4fb587af0b86e3b1457
Bradley Stender <bradleystender@gmail.com>
2018-01-26 13:56:52 -0500

    Finished project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.util.Scanner ;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+
+        int Grade ;
+        Scanner input = new Scanner
+
+        if (Grade < 45) {
+            System.out.println("You have an F");
+        } else if ( Grade == 45) {
+
+        }
+
+        int i;
+
+        for (i = 1; i <= 1000; i++) {
+
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println();
+            } else if (i % 5 == 0) {
+
+            } else {
+                System.out.println(i);
+            }
+
+
+        }
+
+    }}
